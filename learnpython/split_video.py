06abc8a08cdafe64de1579df777806f8a6501bbb
tuanle201097 <tuanle201097@gmail.com>
2024-07-15 01:34:25 +0700

    some updates

--- learnpython/split_video.py
+++ learnpython/split_video.py
@@ -1,27 +1,27 @@
+import os
 import yt_dlp
-import subprocess
 
-def cut_video(video_url, start_time, end_time, output_filename):
-    ydl_opts = {
-        'format': 'bestvideo+bestaudio/best',
-        'outtmpl': '%(title)s.%(ext)s',
-    }
-    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-        info_dict = ydl.extract_info(url, download=False)
-        video_url = info_dict.get('url')
-        ffmpeg_command = [
-            'ffmpeg', '-i', video_url,
-            '-ss', start_time,
-            '-to', end_time,
-            '-c', 'copy',
-            output_filename
-        ]
-        subprocess.run(ffmpeg_command, capture_output=True)
+download_cmd = f'yt-dlp -f 398+140 --merge-output-format mp4  https://www.youtube.com/watch?v=Zjl2vmy02As'
+os.system(download_cmd)
+#rename file da tai
+os.rename(r'D:\python\learnpython\A Day in the Life of a Software Engineer... WFH [Zjl2vmy02As].f140.m4a', r'D:\python\learnpython\audio.m4a')
+os.rename(r'D:\python\learnpython\A Day in the Life of a Software Engineer... WFH [Zjl2vmy02As].f398.mp4', r'D:\python\learnpython\video.mp4')
 
-if __name__ == "__main__":
-    video_url = "https://www.youtube.com/watch?v=Zjl2vmy02As"
-    start_time = "00:01:00"
-    end_time = "00:02:00"
-    output_filename = "output.mp4"
+# #Đường dẫn tuyệt đối tới thư mục chứa file ffmpeg.exe
+ffmpeg_dir = r'D:\python\learnpython\ffmpeg-master-latest-win64-gpl-shared\bin'
+# Chuyển đến thư mục chứa ffmpeg.exe
+os.chdir(ffmpeg_dir)
+audio_file = r'D:\python\learnpython\audio.m4a'
+video_file = r'D:\python\learnpython\video.mp4'
+input_file = r'D:\python\learnpython\input.mp4'
 
-    cut_video(video_url, start_time, end_time, output_filename)
+cmd_merge = f'ffmpeg -i "{video_file}" -i "{audio_file}" -c:v copy -c:a aac "{input_file}"'
+
+os.system(cmd_merge)
+
+# Lệnh cmd để chạy ffmpeg (ví dụ: cắt video)
+output_file = r'D:\python\learnpython\output.mp4'
+cmd_split = f'ffmpeg -i "{input_file}" -ss 00:00:10 -to 00:00:20 -c:v copy -c:a copy "{output_file}"'
+
+# Thực thi lệnh cmd
+os.system(cmd_split)
