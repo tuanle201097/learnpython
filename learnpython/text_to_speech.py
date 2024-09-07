81a7b21ef06314e30b5eb5788ba5e823a774ae70
tuanle201097 <tuanle201097@gmail.com>
2024-09-07 18:38:48 +0700

    update

--- learnpython/text_to_speech.py
+++ learnpython/text_to_speech.py
@@ -2,8 +2,12 @@ from gtts import gTTS
 from pydub import AudioSegment
 import os
 import pyttsx3
+#Đường dẫn tuyệt đối tới thư mục chứa file ffmpeg.exe
+ffmpeg_dir = r'D:\python\learnpython\ffmpeg-master-latest-win64-gpl-shared\bin'
 
-def text_to_speech1(text):
+def text_to_speech1(text_file,mp3_filename):
+    # Chuyển đến thư mục chứa ffmpeg.exe
+    os.chdir(ffmpeg_dir)
     # Khởi tạo engine
     engine = pyttsx3.init()
     # Đặt tốc độ đọc (từ 0 đến 1)
@@ -14,13 +18,14 @@ def text_to_speech1(text):
     for voice in voices:
         print(voice)
     engine.setProperty('voice', voices[2].id)  # Chọn một giọng đọc (index 1 là giọng đọc nam)
-
-    # Chuyển đổi văn bản thành giọng nói
-    engine.say(text)
-
-    # Chờ engine đọc xong
-    engine.runAndWait()
-
+    # Đọc nội dung từ file text
+    with open(text_file, 'r', encoding='utf-8') as file:
+        text = file.read()
+        # Chuyển đổi văn bản thành giọng nói
+        engine.say(text)
+        engine.save_to_file(text,mp3_filename)
+        # Chờ engine đọc xong
+        engine.runAndWait()
 def text_to_speech2(text_file, output_audio_file):
     # Đọc nội dung từ file text
     with open(text_file, 'r', encoding='utf-8') as file:
@@ -32,21 +37,25 @@ def text_to_speech2(text_file, output_audio_file):
     tts.save(output_audio_file)
     print("Done")
 
+# Đặt tên file đầu ra
+wav_filename = "D:\python\learnpython\output.wav"
+mp3_filename = "D:\python\learnpython\output.mp3"
 # Đường dẫn đến file text và file âm thanh đầu ra
-text_file = 'subtitles.txt'
+text_file = "D:\python\learnpython\subtitles.txt"
 output_audio_file = 'output_audio.mp3'
+text_file2 = "Xin chào, đây là một ví dụ về chuyển đổi văn bản thành giọng nói bằng pyttsx3."
 
 #Gọi hàm để chuyển đổi và lưu file âm thanh
-# text_to_speech2(text_file, output_audio_file)
+text_to_speech2(text_file, output_audio_file)
 # Gọi hàm để chuyển đổi và lưu file âm thanh
-# text_to_speech1(text_file)
+# text_to_speech1(text_file, mp3_filename)
 
 
-#Đường dẫn tuyệt đối tới thư mục chứa file ffmpeg.exe
-ffmpeg_dir = r'D:\python\learnpython\ffmpeg-master-latest-win64-gpl-shared\bin'
-# Chuyển đến thư mục chứa ffmpeg.exe
-os.chdir(ffmpeg_dir)
+# #Đường dẫn tuyệt đối tới thư mục chứa file ffmpeg.exe
+# ffmpeg_dir = r'D:\python\learnpython\ffmpeg-master-latest-win64-gpl-shared\bin'
+# # Chuyển đến thư mục chứa ffmpeg.exe
+# os.chdir(ffmpeg_dir)
 
-cmd_speed = 'ffmpeg -i D:\python\learnpython\output_audio.mp3 -filter:a "atempo=1.7" D:\python\learnpython\output.mp3'
-# Thực thi lệnh cmd
-os.system(cmd_speed)
+# cmd_speed = 'ffmpeg -i D:\python\learnpython\output_audio.mp3 -filter:a "atempo=1.7" D:\python\learnpython\output.mp3'
+# # Thực thi lệnh cmd
+# os.system(cmd_speed)
