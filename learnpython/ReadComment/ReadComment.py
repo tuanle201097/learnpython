2ef183373ac4be130af4e3ceacaf1d8dc76f3b7e
tuanle201097 <tuanle201097@gmail.com>
2024-11-30 21:24:31 +0700

    thêm hàm capture ảnh và hàm tìm chữ và số trong ảnh

--- learnpython/ReadComment/ReadComment.py
+++ learnpython/ReadComment/ReadComment.py
@@ -8,6 +8,7 @@ import re
 def CaptureScreen():
     # Khởi tạo đối tượng MSS
     with mss.mss() as sct:
+        screenshot = None
         # Lấy ảnh chụp màn hình và lưu vào đường dẫn chỉ định
         screenshot = sct.shot(output=r"D:\python\learnpython\ReadComment\captures\screenshot.png")
         print("Đã chụp ảnh màn hình và lưu vào file screenshot.png")
@@ -16,41 +17,46 @@ def CaptureScreen():
     pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
 
     # Mở hình ảnh
-    image = Image.open(r"D:\python\learnpython\ReadComment\captures\screenshot.png")
+    image = Image.open(screenshot)
 
     # Lấy danh sách ngôn ngữ có sẵn
-    ngon_ngu = pytesseract.get_available_languages()
+    ngon_ngu = pytesseract.get_languages()
     print("Danh sách ngôn ngữ hỗ trợ:", ngon_ngu)
 
-    # Sử dụng Tesseract để nhận diện chữ
-    text = pytesseract.image_to_string(image)
-    # In kết quả nhận diện
-    print("Văn bản nhận diện được:", text)
+    custom_config = r'-l eng --psm 6'
+    output = pytesseract.image_to_string(image, config=custom_config)
 
-# Gọi hàm CaptureScreen
-CaptureScreen()
 
-def DetectData():
-    img = cv2.imread('D:\python\learnpython\ReadComment\captures\Capture2.PNG')
-    # d = pytesseract.image_to_data(img, output_type=Output.DICT)
-    # keys = list(d.keys())
-
-    # date_pattern1 = '^(0[1-9]|[12][0-9]|3[01])/(0[1-9]|1[012])/(19|20)\d\d$'
-    # date_pattern2 = '111-222-333'
-
-    # n_boxes = len(d['text'])
-    # for i in range(n_boxes):
-    #     if int(d['conf'][i]) > 60:
-    #         (x, y, w, h) = (d['left'][i], d['top'][i], d['width'][i], d['height'][i])
-    #         img = cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
-    	    # if re.match(date_pattern2, d['text'][i]):
-	        #     (x, y, w, h) = (d['left'][i], d['top'][i], d['width'][i], d['height'][i])
-	        #     img = cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
-
-    # cv2.imshow('img', img)
-    # cv2.waitKey(0)
-    custom_config = r'-l vie+eng --psm 6'
-    output = pytesseract.image_to_string(img, config=custom_config)
     print(output)
 
-CaptureScreen()
+def DetectData(date_pattern,word_pattern):
+    img = cv2.imread('D:\\python\\learnpython\\ReadComment\\captures\\Capture2.PNG')
+    d = pytesseract.image_to_data(img, output_type=Output.DICT)
+    
+    n_boxes = len(d['text'])
+    for i in range(n_boxes):
+        if int(d['conf'][i]) > 60:
+            text = d['text'][i]
+
+            # Kiểm tra nếu từ trùng khớp với biểu thức chính quy ngày tháng
+            if re.match(date_pattern, text):
+                print(f"Ngày tháng phát hiện: {text}")
+                (x, y, w, h) = (d['left'][i], d['top'][i], d['width'][i], d['height'][i])
+                img = cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
+            
+            # Kiểm tra nếu từ trùng khớp với từ "File"
+            if re.match(word_pattern, text, re.IGNORECASE):  # Thêm re.IGNORECASE để tìm cả "file" và "File"
+                print(f"Từ 'File' phát hiện: {text}")
+                (x, y, w, h) = (d['left'][i], d['top'][i], d['width'][i], d['height'][i])
+                img = cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)  # Đổi màu cho từ 'File'
+
+    # Hiển thị ảnh với các hình chữ nhật xung quanh từ nhận diện
+    cv2.imshow('img', img)
+    cv2.waitKey(0)
+# CaptureScreen()
+# Biểu thức chính quy tìm ngày tháng
+date_pattern = '^(0[1-9]|[12][0-9]|3[01])/(0[1-9]|1[012])/(19|20)\d\d$'
+# Biểu thức chính quy tìm kiếm từ "File"
+word_pattern = 'File'
+
+DetectData('4','Tesseract')
